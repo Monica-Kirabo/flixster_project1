89bfa02774864504c55704dc1292c0e0b17099da
monica kirabo <kirabom@duq.edu>
2022-06-19 18:47:45 -0700

    commit here

--- script.js
+++ script.js
@@ -1,5 +1,86 @@
 
 
+const movieSearchUrl = "https://api.themoviedb.org/3/search/movie"
+const nowPlayingUrl = "https://api.themoviedb.org/3/search/movie/now_playing";
+const movieImageUrl = "https://api.themoviedb.org/3/movie";
+const apiKey="26a6ca6a064e61eeb4b31ebf6a5a3c01";
+//const apiUrl=" https://api.themoviedb.org/3/search/movie/now_playing"
+const movieArea= document.getElementById('movieArea');
+var currentSearchTerm='';
+const searchInput = document.getElementById('search-input');
+const searchForm = document.getElementById('search-form');
+const IMGPATH= "https://image.tmdb.org/t/p/w1280";
+/*
+window.onload=function(){
+const moviedisplay=document.querySelector('movieArea');
+    moviedisplay.addEventListener(`show`, ()=>{
+        const dis=moviedisplay.value;
+        
+        nowplaying(dis);
+    });
+} 
+*/
+function nowplaying(nowPlayingUrl){
+var result=fetch(nowPlayingUrl + "?api_key=" + apikey).then(response => response.json())
+  .then(data => console.log(data));
+  console.log(result);
+}
+async function getdata(searchTerm){
+ 
+    
+//const offset = currentApiPage * pageSize;
+const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key= ${apiKey}&q=${searchTerm}`).then(response => response.json())
+  .then(data => console.log(data));
+  return response;
+
+}
+
+function generateHTML(movies){
+movies.foreach(data=>{
+    const  movieTitle=document.createElement('p');
+    movieTitle.innerHTML=`${data.original_title}`;
+  const  movieRating=document.createElement('p');
+    movieRating.innerHTML=`${data.original_Rating}`;
+    movieTitle.append(movieTitle);
+    movieRating.append(movieRating);
+});
+movieArea.innerHTML=output;
+}
+async function handleFormSubmit(event) {
+    event.preventDefault();
+   // movieArea.innerHTML = '';
+    currentSearchTerm = searchInput.value;
+    const results = await getdata(currentSearchTerm);
+    generateHTML(results);
+    searchInput.value = '';
+    currentApiPage++;
+    showMeMoreBtn.classList.remove('hidden');
+}
+
+searchForm.addEventListener('submit', handleFormSubmit);
+
+function formSubmit(ev){
+    var formel=document.querySelector("form");
+    formel.addEventListener("submit",(ev)=>{
+        console.log(ev.target.searchTerm.value);
+        getdata(ev.target.searchTerm.value);
+    });
+}
+const movieAr=document.querySelector('movieArea');
+movieAr.addEventListener(`submit`,formSubmit(onclick))
+
+function generateMore(movie){
+    return '<img src="${movie.img}">
+            <span>${movie.name}</span>
+            <span>${movie.rating}</span> 
+            ' 
+
+}
+
+
+
+
+/*
 const base_url='https://www.themoviedb.org/';
 const posterPath='/kqjL17yufvn9OVLyXYpvtyrFfak.jpg';
 const posterImage='https://image.tmdb.org/t/p/w500';
@@ -73,3 +154,4 @@ showMeMoreBtn.addEventListener('click', handleShowMeMoreClick);
 window.onload=function(){
     getmovies(API_URL);
 }
+*/
